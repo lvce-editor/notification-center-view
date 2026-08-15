7242405c51d35d83d00b5a6237c2ed40737110c5
Le Vivilet <levivilet@pm.me>
2026-08-15 06:49:23 -0700

    Keep notification render paths independent (#13)
    * feature: render directly in renderer process
    
    * fix: commit out-of-band notification renders
    
    * fix: keep notification render paths independent

--- packages/notification-center-view/src/parts/Render2/Render2.ts
+++ packages/notification-center-view/src/parts/Render2/Render2.ts
@@ -1,26 +1,12 @@
 import { ViewletCommand } from '@lvce-editor/constants'
 import { getNotificationCenterVirtualDom } from '../GetNotificationCenterVirtualDom/GetNotificationCenterVirtualDom.ts'
 import * as NotificationCenterStates from '../NotificationCenterStates/NotificationCenterStates.ts'
-import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 
-const renderCommands = (uid: number, diffResult: readonly number[]): readonly unknown[] => {
+export const render2 = (uid: number, diffResult: readonly number[]): readonly unknown[] => {
   const { scheduledState } = NotificationCenterStates.get(uid)
   NotificationCenterStates.set(uid, scheduledState, scheduledState)
   if (diffResult.length === 0) {
     return []
   }
   return [[ViewletCommand.SetDom2, uid, getNotificationCenterVirtualDom(scheduledState.notifications)]]
-}
-
-export const render2 = (uid: number, diffResult: readonly number[]): readonly unknown[] | Promise<readonly unknown[]> => {
-  const commands = renderCommands(uid, diffResult)
-  if (!RendererProcess.isConnected()) {
-    return commands
-  }
-  return renderDirect(uid, commands)
-}
-
-const renderDirect = async (uid: number, commands: readonly unknown[]): Promise<readonly unknown[]> => {
-  const transactionId = await RendererProcess.invoke('Viewlet.queueCommands', uid, commands)
-  return [['Viewlet.commitPending', uid, transactionId]]
 }
