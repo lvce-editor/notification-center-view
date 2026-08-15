a03c260d0575e940acfe28e251f1a52848435070
Le Vivilet <levivilet@pm.me>
2026-08-15 06:43:21 -0700

    Commit out-of-band notification renders (#12)
    * feature: render directly in renderer process
    
    * fix: commit out-of-band notification renders

--- packages/notification-center-view/src/parts/RenderOutOfBand/RenderOutOfBand.ts
+++ packages/notification-center-view/src/parts/RenderOutOfBand/RenderOutOfBand.ts
@@ -1,13 +1,13 @@
 import { RendererProcess } from '@lvce-editor/rpc-registry'
 import { diff2 } from '../Diff2/Diff2.ts'
-import { renderCommands } from '../Render2/Render2.ts'
+import { render2 } from '../Render2/Render2.ts'
 
 export const renderOutOfBand = async (uid: number): Promise<void> => {
   const diffResult = diff2(uid)
   if (diffResult.length === 0) {
     return
   }
-  const commands = renderCommands(uid, diffResult)
+  const commands = await render2(uid, diffResult)
   if (commands.length === 0) {
     return
   }
